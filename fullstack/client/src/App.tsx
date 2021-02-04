52b1d29ef2bb042d01b114d28b343f22a8ac7cce
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2021-02-04 22:44:10 +0400

    configuration terminal and socket on client side

--- fullstack/client/src/App.tsx
+++ fullstack/client/src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { io } from 'socket.io-client';
+import { XTerm } from 'xterm-for-react';
+import { FitAddon } from 'xterm-addon-fit';
+
 import './App.css';
 
+const SERVER_URI = 'http://127.0.0.1:8888';
+const socket = io(SERVER_URI);
+const fitAddon = new FitAddon();
+
 function App() {
+  const xtermRef = useRef<any>();
+
+  useEffect(() => {
+    fitAddon.fit(); // fullscreen mode for terminal
+    socket.on('data', (data: any) => {
+      xtermRef.current.terminal.write(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <h2>Hello, World!</h2>
+    <div className="app">
+      <XTerm
+        ref={xtermRef}
+        addons={[fitAddon]}
+        onData={(e) => {
+          socket.emit('command', e);
+        }}
+      />
     </div>
   );
 }
